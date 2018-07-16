2de045afae2b0a14613f3d1dec211fbd30b1a32a
liambot <liambot@github.com>
2018-07-16 09:46:57 -0400

    update

--- js/java.js
+++ js/java.js
@@ -21,3 +21,19 @@ function openCity(evt, cityName) {
 
 
 }
+
+document
+  .querySelector('.js-change-theme')
+  .addEventListener('click', () => {
+    const body = document.querySelector('body');
+
+    if (body.classList.contains('t--light')) {
+      body.classList.remove('t--light');
+      body.classList.add('t--dark');
+    }
+    else {
+      body.classList.remove('t--dark');
+      body.classList.add('t--light');
+    }
+  })
+;
