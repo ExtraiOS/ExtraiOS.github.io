2cbf503f35adb4511ae54fa06227c45ec684cc32
liambot <liambot@github.com>
2018-07-25 14:55:02 -0400

    update

--- js/java.js
+++ js/java.js
@@ -19,8 +19,24 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 
 
+  // When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
 
-}
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("myBtn").style.display = "block";
+	    } else {
+	        document.getElementById("myBtn").style.display = "none";
+	    }
+
+
+	// When the user clicks on the button, scroll to the top of the document
+	    document.body.scrollTop = 0; // For Safari
+	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+
+
+
+
 
 document
   .querySelector('.js-change-theme')
@@ -73,3 +89,26 @@ function carousel() {
 
 var d = new Date();
 document.getElementById("demo").innerHTML = d.toDateString();
+
+$(document).on('click', 'a', function (e) {
+    if ($('#'+e.target.id).attr('href')=='#') {
+        e.preventDefault();
+    }
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
