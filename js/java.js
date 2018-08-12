1e494685eb7ba0d5d620c7da8c48fb32910ea21b
liambot <liambot@github.com>
2018-08-12 14:50:45 -0400

    update

--- js/java.js
+++ js/java.js
@@ -128,3 +128,83 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+	//Open Home Page First
+
+	// Get the element with id="defaultOpen" and click on it
+	document.getElementById("defaultOpen").click();
+
+
+	//Dark Mode
+
+
+	document
+	 .querySelector('.js-change-theme')
+	 .addEventListener('click', () => {
+	   const body = document.querySelector('body');
+
+	   if (body.classList.contains('t--light')) {
+	     body.classList.remove('t--light');
+	     body.classList.add('t--dark');
+	   }
+	   else {
+	     body.classList.remove('t--dark');
+	     body.classList.add('t--light');
+	   }
+	 })
+	;
+
+
+	//Home Page Gallery
+
+
+	var slideIndex = 0;
+	carousel();
+
+	function carousel() {
+	   var i;
+	   var x = document.getElementsByClassName("mySlides");
+	   for (i = 0; i < x.length; i++) {
+	     x[i].style.display = "none";
+	   }
+	   slideIndex++;
+	   if (slideIndex > x.length) {slideIndex = 1}
+	   x[slideIndex-1].style.display = "block";
+	   setTimeout(carousel, 6000);
+	}
+
+
+	//Home Page Date
+
+
+	var d = new Date();
+	document.getElementById("demo").innerHTML = d.toDateString();
+
+
+	//Apps Search Bar
+
+
+	function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+	}
+
+
+	//FONT
+
+
+	$(document).ready( function() {
+				$('#deletesuccess').delay(8000).fadeOut();
+			});
